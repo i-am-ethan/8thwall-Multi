890863c315795e2bf83f59b05d10f5bd233df98d
ethan <izawaryosuke0101@gmail.com>
2021-09-25 01:30:50 +0900

    サーバー側コネクション

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express'); //expressモジュールの読み込み
 const app = express(); //expressアプリケーションの作成
 const http = require('http').Server(app); //httpモジュールの読み込み
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
+
 
 
 http.listen(PORT, ()=>{
@@ -15,4 +17,9 @@ app.get('/', (req, res)=>{
 
 app.get('/multi', (req, res)=>{
     res.sendFile(__dirname + '/multi.html')
+})
+
+
+io.on('connection', (socket)=>{
+    console.log("connected")
 })
