8c8cce7b68256ea66bb77de6e3b52ab1b33fd521
ethan <izawaryosuke0101@gmail.com>
2021-09-24 19:01:00 +0900

    server.js作成してlocalhost立ち上げ

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'); //expressモジュールの読み込み
+const app = express(); //expressアプリケーションの作成
+const http = require('http').Server(app); //httpモジュールの読み込み
+const PORT = process.env.PORT || 3000;
+
+
+http.listen(PORT, ()=>{
+    console.log('server listening..... Port:' + PORT)
+})
+
+
+app.get('/', (req, res)=>{
+    res.send('hello world')
+})
