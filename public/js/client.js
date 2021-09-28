e3781e045c44f340afec26646676c0ec73c26fa8
ethan <izawaryosuke0101@gmail.com>
2021-09-28 15:36:35 +0900

    no message

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,1 @@
-export default ()=>{
-    console.log("client.js reloaded")
-}
+console.log("client.js reloaded")
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,1 @@
-'use strict';
-
-import client from './client'
-
-client();
-
 console.log("index.js reloaded")
