9673c18bc91c7a8c363787d29346f449bde1410a
ethan <izawaryosuke0101@gmail.com>
2021-09-29 16:44:21 +0900

    no message

--- public/js/client-side.js
+++ public/js/client-side.js
@@ -3,22 +3,22 @@ const debugConsole = document.getElementById("debug-console")
 
 
 // room-id取得
-let clientSearch = location.search
-console.log(clientSearch)
-clientSearchArray = clientSearch.split("=")
-console.log(clientSearchArray)
+let clientSearch = location.search // urlの?以下取得
+clientSearchArray = clientSearch.split("=") //=で分割
 clientSearchArray.shift() //先頭を削除
-console.log(clientSearchArray)
+let clientRoomId = clientSearchArray[0]; // room-idだけ取得
 
-let clientRoomId = clientSearchArray[0];
-console.log("client-room-id:"+clientRoomId)
-//?room=8080
 
 
 const socket = io.connect();
+
+
 
 socket.on("connect", () => {
     const debugConsole02 = document.createElement("p")
     debugConsole02.innerHTML = "client-id:" + socket.id;
     debugConsole.appendChild(debugConsole02)
+    const debugConsoleRoomId = document.createElement("p")
+    debugConsoleRoomId.innerHTML = "your room-id:"+ clientRoomId;
+    debugConsole.appendChild(debugConsoleRoomId)
 })
