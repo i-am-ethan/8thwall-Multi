d1e0c37cdd37526a9950183d0fc11ddd27ba6f8c
ethan <izawaryosuke0101@gmail.com>
2021-09-29 14:15:50 +0900

    no message

--- public/js/client-side.js
+++ public/js/client-side.js
@@ -9,5 +9,8 @@ console.log("client.js reloaded")
 const socket = io.connect();
 
 socket.on("connect", () => {
+    const debugConsole02 = document.createElement("p")
+    debugConsole02.innerHTML = socket.id
+    debugConsole.appendChild(debugConsole02)
     console.log(socket.id)
 })
