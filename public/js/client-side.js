01f6dcba20c527463968b786e3c67303e840390c
ethan <izawaryosuke0101@gmail.com>
2021-10-04 12:01:05 +0900

    no message

--- public/js/client-side.js
+++ public/js/client-side.js
@@ -91,7 +91,7 @@ AFRAME.registerComponent('random-cube-generator', {
             
             const newCube = document.createElement("a-box")
             newCube.setAttribute("color", `${randomCubeColor[random]}`)
-            newCube.setAttribute("scale", "3 3 3")
+            newCube.setAttribute("scale", "1 1 1")
             // newCube.object3D.position.set(3, 0, 0)
             newCube.setAttribute('position', camera.object3D.position)
             newCube.setAttribute("class", "cantap")
