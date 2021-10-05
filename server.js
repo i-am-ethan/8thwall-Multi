86114835ba54bcf32efc524c61485be09eb74f33
ethan <izawaryosuke0101@gmail.com>
2021-10-06 03:29:46 +0900

    no message

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ io.on('connection', (socket)=>{
     socket.on('join_room', (data)=>{
         roomid = data;
         socket.join(roomid);
-        io.broadcast.to(roomid).emit('join_room', data);
-        // io.to(roomid).emit('join_room', data);
+        // io.broadcast.to(roomid).emit('join_room', data);
+        io.to(roomid).emit('join_room', data);
     })
 
     // clientからgenerate_boxが送信された時
