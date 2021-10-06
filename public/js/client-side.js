991fbcfa94c9194a5e28a5e30c7aca9e9288d2ae
ethan <izawaryosuke0101@gmail.com>
2021-10-07 00:01:15 +0900

    no message

--- public/js/client-side.js
+++ public/js/client-side.js
@@ -95,7 +95,7 @@ AFRAME.registerComponent('random-cube-generator', {
             let socketdata = {} //socket-data objectを生成
             let cube = [] //cube配列生成
             cube.push({color: "#ff3300"}) //cube配列に押し込んでいく
-            cube.push({position: newCube.object3D.position.x +" "+newCube.object3D.position.y+" "+newCube.object3D.position.z}) //cube配列に押し込んでいく
+            cube.push({position: newCube.object3D.position.x +" ,"+newCube.object3D.position.y+", "+newCube.object3D.position.z}) //cube配列に押し込んでいく
             cube.push({scale: "3 3 3"});
             cube.push({id: `created-box${randomID}`});
             socketdata["cube"] = cube; //socket-dataオブジェクトにcubeを押し込む
