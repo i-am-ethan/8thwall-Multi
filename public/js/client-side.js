849dc4e73503dcff69b88a5e31c60521cf144689
ethan <izawaryosuke0101@gmail.com>
2021-10-06 21:14:47 +0900

    no message

--- public/js/client-side.js
+++ public/js/client-side.js
@@ -134,7 +134,9 @@ AFRAME.registerComponent('random-cube-generator', {
             console.log("どのid名のcubeを取得したか!!!!!"+JSON.stringify(cube[1]))
             console.log("どのid名のcubeを取得したか!!!!!"+JSON.stringify(cube[1].id))
             let updateCubeID = JSON.stringify(cube[1].id)
+            let replaced = updateCubeID.replace('""', '')
             console.log(updateCubeID)
+            console.log(replaced)
             let testID ="test"
             console.log(testID)
             // let updateCube = document.getElementById(JSON.stringify(cube[1].id));
